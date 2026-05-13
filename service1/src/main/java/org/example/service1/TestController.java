7afe22fabaf4dc736468992c9dae3dfe98956902
Martin Blomberg <martin.blomberg@outlook.com>
2026-05-13 11:56:03 +0200

    Refactor TestController for constructor-based dependency injection and update Spring Boot configuration in service1

--- service1/src/main/java/org/example/service1/TestController.java
+++ service1/src/main/java/org/example/service1/TestController.java
@@ -3,7 +3,6 @@ package org.example.service1;
 import org.example.grpc.GreetingServiceGrpc;
 import org.example.grpc.HelloRequest;
 import org.example.grpc.HelloResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Autowired
-    GreetingServiceGrpc.GreetingServiceBlockingStub stub;
+    final GreetingServiceGrpc.GreetingServiceBlockingStub stub;
+
+    public TestController(GreetingServiceGrpc.GreetingServiceBlockingStub stub) {
+        this.stub = stub;
+    }
 
     @GetMapping("/api/test")
     public String test(@RequestParam(defaultValue = "Guest") String name) {
         HelloRequest request = HelloRequest.newBuilder()
                 .setName(name)
                 .build();
-
         HelloResponse response = stub.sayHello(request);
 
         return "Service 1 received gRPC response: " + response.getMessage();
